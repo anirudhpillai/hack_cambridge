e945ae69b434a38ea8543f0fc60d7653d17fa139
abdulfaizp <abdulfaizp@gmail.com>
2017-01-29 14:41:46 +0000

    push

--- game/bullet.py
+++ game/bullet.py
@@ -1,10 +1,17 @@
 from math import atan2, cos, sin
 from .constants import *
 import uuid
+import random
+import string
+
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 class Bullet:
     def __init__(self, x, y, dest_x, dest_y):
-        self.bullet_id = uuid.uuid5(uuid.NAMESPACE_DNS, 'game')
+        self.bullet_id = id_generator(10)
         self.x = x
         self.y = y
         self.dest_x = dest_x
