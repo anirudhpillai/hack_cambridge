326b20bd3e6c11ca34cb99f2559d41ca037b606f
Anirudh <anirudhx5@gmail.com>
2017-01-29 09:03:43 +0000

    disable cors

--- fe/webpack.config.js
+++ fe/webpack.config.js
@@ -1,6 +1,6 @@
 var config = {
    entry: './main.js',
-	
+
    output: {
       path:'./',
       filename: 'index.js',
@@ -10,14 +10,14 @@ var config = {
       inline: true,
       port: 8080
    },
-	
+
    module: {
       loaders: [
          {
             test: /\.jsx?$/,
             exclude: /node_modules/,
             loader: 'babel',
-				
+
             query: {
                presets: ['es2015', 'react']
             }
