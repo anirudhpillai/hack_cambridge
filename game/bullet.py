f92f68763e6b4314c6c834bdd9d3f1c71e6af055
Anirudh <anirudhx5@gmail.com>
2017-01-29 14:38:14 +0000

    demo

--- game/bullet.py
+++ game/bullet.py
@@ -13,6 +13,6 @@ class Bullet:
     def update_bullet_position(self):
         direction = atan2(-1 * (self.dest_y - self.y), self.dest_x - self.x)
         print("direction = ", direction)
-        new_x = self.x + cos(direction) * 10
-        new_y = self.y + sin(direction) * 10
+        new_x = self.x + cos(direction) * 50
+        new_y = self.y + sin(direction) * 50
         self.x, self.y = new_x, new_y
